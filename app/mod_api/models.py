219c3ce6695125227bc683f0210e4f5dcabc7737
Ashley Collinge <ashleyc@localhost.localdomain>
2018-11-13 22:05:16 +0000

    added more tables in

--- app/mod_api/models.py
+++ app/mod_api/models.py
@@ -1,49 +1,82 @@
 from app import db
 
 class Asset(db.Model):
-    asset_id      = db.Column(db.Integer, primary_key=True)
+    """
+    Every asset managed by the system has a record in here, which contains the
+    very basic information, the same for every asset. Then there are different
+    asset types which are managed, e.g. Asset_PC and Asset_Monitor.
+    """
+    id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
                                            onupdate=db.func.current_timestamp())
-    computer_name
     status
-    domain
-    location_id
+    location_id = 
     serialnumber
     purchaseordernumber
+    type
 
+class Asset_PC(db.Model):
+    """
+    This contains records for all PC assets held in the estate. Each record
+    has a relationship with the Asset ID in [dbo].[Asset]. This makes it easier
+    when it comes to taking the machine off the domain; it still exists as an
+    asset and needs to be managed - separate from the domain.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
+                                           onupdate=db.func.current_timestamp())
+    hostname = db.Column(db.String)
+    domain = db.Column(db.String)
+    operating_system = db.Column(String)
+    service_pack_version = db.Column(String)
+    last_bootup_time = db.Column(db.DateTime)
 
+class Asset_Location(db.Model):
+    """
+    Record for each location that can exist for an Asset to be stored.
+    Can add notes for each location.
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    notes = db.Column(db.String)
 
-location_id
-name
+class Asset_PC_NetInt(db.Model):
+    """
+    Record for each network interface on each PC. 
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    ip_address = db.Column(db.String)
+    mac_address = db.Column(db.String)
+    subnet_mask = db.Column(db.String)
+    gateway = db.Column(db.String)
+    asset_pc_id = 
 
+class Asset_PC_PhysDisk(db.Model):
+    """
+    Record for each physical disk on each PC
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String)
+    freespace = db.Column(db.Integer)
+    totalsize = db.Column(db.Integer)
+    mediatype = db.Column(db.String)
+    status = db.Column(db.String)
+    asset_pc_id = 
 
-tables of info
-
-//network info
-net_int_id
-ip_address
-mac_address
-subnet_mask
-gateway
-asset_id
-
-//disk info
-physical_disk_id
-caption
-freespace
-totalsize
-mediatype
-status
-
-logical_disk_id
-caption
-freespace
-physical_disk_id
-totalsize
-filesystem
-mediatype
-partition_type
+class Asset_PC_LogDisk(db.Model):
+    """
+    Record for each logical disk on each pc
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String)
+    freespace = db.Column(db.Integer)
+    physical_disk_id
+    totalsize = db.Column(db.Integer)
+    filesystem = db.Column(db.String)
+    mediatype = db.Column(db.String)
+    partition_type = db.Column(db.String)
 
 //software
 software_id
