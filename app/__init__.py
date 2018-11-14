4c1110e1950f6feee2b549a7c51b063ebdd475af
Ashley Collinge <ashleyc@localhost.localdomain>
2018-11-14 20:28:47 +0000

    fix issue around imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from app.mod_auth.controllers import mod_auth as auth_module
-from app.mod_api.controllers import mod_api as api_module
-from app.mod_frontend.controllers import mod_frontend as frontend_module
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -14,6 +12,10 @@ db = SQLAlchemy(app)
 def not_found(error):
     return render_template('404.html'), 404
 
+
+from app.mod_auth.controllers import mod_auth as auth_module
+from app.mod_api.controllers import mod_api as api_module
+from app.mod_frontend.controllers import mod_frontend as frontend_module
 
 # Register blueprint(s)
 app.register_blueprint(auth_module)
