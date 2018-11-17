6aa89b381549fcb77514173459f170645d351afc
Ashley Collinge <ashley.collinge@synseal.com>
2018-11-17 22:10:57 +0000

    testing

--- windows_client/test_file.py
+++ windows_client/test_file.py
@@ -1,4 +1,7 @@
 import requests
 
-r = requests.get("http://127.0.0.1:5000/api/assets")
-print(r.json())
+r = requests.post("http://127.0.0.1:5000/agent/register")
+print(r.content)
+
+r = requests.post("http://127.0.0.1:5000/agent/heartbeat")
+print(r.content)
