15faf68e6fd71d4e32213fcd93e7884177d1e1af
Ashley Collinge <ashley.collinge@synseal.com>
2018-11-17 23:27:01 +0000

    started to implement the process agents take to register

--- app/mod_agent/controllers.py
+++ app/mod_agent/controllers.py
@@ -1,20 +1,48 @@
 # Import flask dependencies
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, url_for
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 mod_agent = Blueprint('agent', __name__, url_prefix='/agent')
 
 
+@mod_agent.route('/configuration', methods=['GET'])
+def get_configuration():
+    """
+    allows agent to get the latest configuration that they
+    need to adhere too
+    """
+    return jsonify({"configuration": {"heartbeat_interval": 2,
+                                      "heartbeat_uri": "http://127.0.0.1:5000/agent/heartbeat"}})
+
+
 # Set the route and accepted methods
 @mod_agent.route('/register', methods=['POST'])
 def signin():
+    """
+    Takes in serial number of the machine and it's ip address,
+    and name
+    creates a new asset record from that infomration and creates
+    an agent record tied to that asset.
+    returns an id, api token, uri to pull down agent config
+    """
+    if request.method == 'POST':
+        if request.get_json():
+            json_data = request.get_json()
+            if 'serialnumber' in json_data.keys() and 'ipaddress' in json_data.keys():
+                # create new asset here, create new agent here and attach agent to asset
+                # also return the id and the uri to pick up the config
+                return jsonify({"asset_id": "nf3f28hf892", "config_uri": "{}".format(url_for('.get_configuration'))})
     return "Hello"
 
 
 @mod_agent.route('/heartbeat', methods=['POST'])
 def heartbeat():
     """
-    Heartbeat from asset agent
+    recieves id, api token an 'ok' message
+    logs heartbeat to db and return 'ok' message
     """
-    return "Thanks"
+    if request.get_json():
+        json_data = request.get_json()
+        if 'asset_id' in json_data.keys():
+            return jsonify({"message": "Heartbeat recieved from: {}".format(json_data['asset_id'])})
 
--- windows_client/test_file.py
+++ windows_client/test_file.py
@@ -1,7 +1,28 @@
-import requests
+import requests, json, time
 
-r = requests.post("http://127.0.0.1:5000/agent/register")
-print(r.content)
+print("Registering as a client")
 
-r = requests.post("http://127.0.0.1:5000/agent/heartbeat")
-print(r.content)
+
+data = {"serialnumber": "hosefn93hr283n", "ipaddress": "192.168.0.56"}
+headers = {'Content-type': 'application/json'}
+r = requests.post("http://127.0.0.1:5000/agent/register", data=json.dumps(data), headers=headers)
+asset_id = r.json()['asset_id']
+config_uri = r.json()['config_uri']
+
+print("Registered")
+print(asset_id)
+print("Pulling down configuration")
+
+r = requests.get("http://127.0.0.1:5000{}".format(config_uri))
+heartbeat_interval = r.json()['configuration']['heartbeat_interval']
+heartbeat_uri = r.json()['configuration']['heartbeat_uri']
+
+print("Heartbeat Interval: {}".format(heartbeat_interval))
+print("Start heartbeating...")
+
+while 1:
+    time.sleep(heartbeat_interval)
+    data = {"asset_id": asset_id}
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(heartbeat_uri, data=json.dumps(data), headers=headers)
+    print(r.json()['message'])
