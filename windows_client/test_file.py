8dacdcd96d7c4b887549d022998044ad35ff92d9
Ashley Collinge <ashley.collinge@synseal.com>
2018-11-18 01:21:35 +0000

    made it a little bit more reactive to change

--- windows_client/test_file.py
+++ windows_client/test_file.py
@@ -1,28 +1,41 @@
-import requests, json, time
+import requests, json, time,os
 
-print("Registering as a client")
+def writeconfigtofile(config):
+    with open('config.json', 'w') as f:
+        json.dump(config, f)
+config = ""
+if os.path.isfile('config.json'):
+    with open('config.json', 'r') as f:
+        config = json.load(f)
 
 
-data = {"serialnumber": "hosefn93hr283n", "ipaddress": "192.168.0.56"}
-headers = {'Content-type': 'application/json'}
-r = requests.post("http://127.0.0.1:5000/agent/register", data=json.dumps(data), headers=headers)
-asset_id = r.json()['asset_id']
-config_uri = r.json()['config_uri']
+if config:
+    config_uri = "agent/configuration"
+    r = requests.get("http://127.0.0.1:5000/{}".format(config_uri))
+    config = r.json()['configuration']
+    writeconfigtofile(config)
+else:
+    data = {"serialnumber": "hosefn93hr283n", "hostname": "host01", "domain": "test.com", "operating_system": "win7", "servicepackversion": 4}
+    headers = {'Content-type': 'application/json'}
+    r = requests.post("http://127.0.0.1:5000/agent/register", data=json.dumps(data), headers=headers)
+    asset_id = r.json()['asset_id']
+    config_uri = r.json()['config_uri']
 
-print("Registered")
-print(asset_id)
-print("Pulling down configuration")
 
-r = requests.get("http://127.0.0.1:5000{}".format(config_uri))
-heartbeat_interval = r.json()['configuration']['heartbeat_interval']
-heartbeat_uri = r.json()['configuration']['heartbeat_uri']
+    r = requests.get("http://127.0.0.1:5000{}".format(config_uri))
+    config = r.json()['configuration']
+    writeconfigtofile(config)
 
-print("Heartbeat Interval: {}".format(heartbeat_interval))
+
+print("Heartbeat Interval: {}".format(config['heartbeat_interval']))
 print("Start heartbeating...")
 
 while 1:
-    time.sleep(heartbeat_interval)
-    data = {"asset_id": asset_id}
+    time.sleep(config['heartbeat_interval'])
+    data = {"asset_id": config['asset_id']}
     headers = {'Content-type': 'application/json'}
-    r = requests.post(heartbeat_uri, data=json.dumps(data), headers=headers)
-    print(r.json()['message'])
+    try:
+        r = requests.post(config['heartbeat_uri'], data=json.dumps(data), headers=headers)
+        print(r.json()['message'])
+    except:
+        pass
