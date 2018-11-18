baa83eec4378a3b1765df48346acc955877f27e6
Ashley Collinge <ashley.collinge@synseal.com>
2018-11-18 01:43:50 +0000

    made registering process more resilient

--- windows_client/test_file.py
+++ windows_client/test_file.py
@@ -3,6 +3,21 @@ import requests, json, time,os
 def writeconfigtofile(config):
     with open('config.json', 'w') as f:
         json.dump(config, f)
+
+def start_heartbeating(interval):
+    print("Heartbeat Interval: {}".format(interval))
+    print("Start heartbeating...")
+
+    while 1:
+        time.sleep(interval)
+        data = {"asset_id": config['asset_id']}
+        headers = {'Content-type': 'application/json'}
+        try:
+            r = requests.post(config['heartbeat_uri'], data=json.dumps(data), headers=headers)
+            print(r.json()['message'])
+        except requests.exceptions.ConnectionError:
+            print("Could not connect to service")
+
 config = ""
 if os.path.isfile('config.json'):
     with open('config.json', 'r') as f:
@@ -11,31 +26,39 @@ if os.path.isfile('config.json'):
 
 if config:
     config_uri = "agent/configuration"
-    r = requests.get("http://127.0.0.1:5000/{}".format(config_uri))
-    config = r.json()['configuration']
-    writeconfigtofile(config)
-else:
-    data = {"serialnumber": "hosefn93hr283n", "hostname": "host01", "domain": "test.com", "operating_system": "win7", "servicepackversion": 4}
-    headers = {'Content-type': 'application/json'}
-    r = requests.post("http://127.0.0.1:5000/agent/register", data=json.dumps(data), headers=headers)
-    asset_id = r.json()['asset_id']
-    config_uri = r.json()['config_uri']
-
-
-    r = requests.get("http://127.0.0.1:5000{}".format(config_uri))
-    config = r.json()['configuration']
-    writeconfigtofile(config)
-
-
-print("Heartbeat Interval: {}".format(config['heartbeat_interval']))
-print("Start heartbeating...")
-
-while 1:
-    time.sleep(config['heartbeat_interval'])
-    data = {"asset_id": config['asset_id']}
-    headers = {'Content-type': 'application/json'}
     try:
-        r = requests.post(config['heartbeat_uri'], data=json.dumps(data), headers=headers)
-        print(r.json()['message'])
-    except:
-        pass
+        r = requests.get("http://127.0.0.1:5000/{}".format(config_uri))
+        config = r.json()['configuration']
+        writeconfigtofile(config)
+        start_heartbeating(config['heartbeat_interval'])
+    except requests.exceptions.ConnectionError:
+        print("Could not connect to service")
+
+data = {"serialnumber": "hosefn93hr283n", "hostname": "host01", "domain": "test.com", "operating_system": "win7", "servicepackversion": 4}
+headers = {'Content-type': 'application/json'}
+registered = False
+no_config = False
+while not registered:
+    time.sleep(2)
+    try:
+        r = requests.post("http://127.0.0.1:5000/agent/register", data=json.dumps(data), headers=headers)
+        asset_id = r.json()['asset_id']
+        config_uri = r.json()['config_uri']
+        registered = True
+    except requests.exceptions.ConnectionError:
+        print("Could not connect to service")
+        print("Trying again...")
+while not no_config:
+    time.sleep(2)
+    try:
+        r = requests.get("http://127.0.0.1:5000{}".format(config_uri))
+        config = r.json()['configuration']
+        writeconfigtofile(config)
+        no_config = True
+    except requests.exceptions.ConnectionError:
+        print("Could not connect to service")
+        print("Trying again...")
+
+start_heartbeating(config['heartbeat_interval'])
+
+
