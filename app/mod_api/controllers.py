616aca3acc1a26a3c60909e07adfde84686d2e0a
Ashley Collinge <ashley.collinge@synseal.com>
2018-11-21 21:07:33 +0000

    sorted out cpu model and turned an int to str

--- app/mod_api/controllers.py
+++ app/mod_api/controllers.py
@@ -106,6 +106,7 @@ def get_extended_info(asset_Id):
     extended_info['cpu_maxclockspeed'] = asset.assetpcrecord[0].cpu_maxclockspeed
     extended_info['cpu_logicalcorecount'] = asset.assetpcrecord[0].cpu_logicalcorecount
     extended_info['cpu_physicalcorecount'] = asset.assetpcrecord[0].cpu_physicalcorecount
+    extended_info['cpu_model'] = asset.assetpcrecord[0].cpu_model
     extended_info['physical_arch'] = asset.assetpcrecord[0].physical_arch
     new_asset['extended_info'] = extended_info
     json_results.append(new_asset)
